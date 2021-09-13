ebaf2faf1c90aad8de90b9a809748a8b282b04fe
Gadamment <josecarvalhobneto@gmail.com>
2021-09-13 17:44:06 -0300

    first commit

---
+++ Identificador_de_imagens/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Identificador_de_imagens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
